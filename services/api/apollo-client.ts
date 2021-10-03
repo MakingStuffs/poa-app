aacdc9ac27efb8dbc0bc3abe0630a7b916e09bd1
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-10-03 13:14:58 +0100

    -- Restructure project so that it makes a bit more sense. -- All state related modules/logic is now in store -- All services to help with getting data etc are in services -- All modules that are generic utilities are in utils

--- services/api/apollo-client.ts
+++ services/api/apollo-client.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { env } from "constants/index";
+import { env } from "utils/constants/index";
 
 const client = new ApolloClient({
   uri: `http://localhost:1337/graphql`,
--- state/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as authInitialState } from "./authInitialState";
