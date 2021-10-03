00a65078bed10b0e0752a4fb2dfd2da5b1b19ec1
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-10-03 15:49:53 +0100

    [3, 7] -- Add definition for GQL_URL in constants env exporter

--- utils/constants/environment.ts
+++ utils/constants/environment.ts
@@ -1,8 +1,9 @@
 import { Environment } from "types";
 
-const { NEXT_PUBLIC_API_URL } = process.env;
+const { NEXT_PUBLIC_API_URL, NEXT_PUBLIC_GQL_URL } = process.env;
 
 const env: Environment = {
   API_URL: NEXT_PUBLIC_API_URL || "http://localhost:1337",
+  GQL_URL: NEXT_PUBLIC_GQL_URL || "http://localhost:1337/graphql",
 };
 export default env;
