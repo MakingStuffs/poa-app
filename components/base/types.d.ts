254579d2785f74286b4763019827c99bb1ade335
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-10-20 07:52:14 +0100

    -- Move component type definitions into the base components folder as it makes more sense in here

---
+++ components/base/types.d.ts
@@ -0,0 +1,13 @@
+export interface FormProps extends React.HTMLAttributes<HTMLFormElement> {
+  onSubmit: React.FormEventHandler;
+}
+
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  clickHandler: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface DropDownProps extends React.HTMLAttributes<HTMLSelectElement> {
+  changeCallback?: (event: React.ChangeEvent) => void;
+}
