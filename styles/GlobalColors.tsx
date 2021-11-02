f4f2d47625ad503a2a4d7efc327be6af25ec30a2
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-11-02 07:16:11 +0000

    -- Added core site colors

--- styles/GlobalColors.tsx
+++ styles/GlobalColors.tsx
@@ -1,3 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle``;
+export default createGlobalStyle`
+    :root {
+        --primary: #3454D1;
+        --secondary: #34D1BF;
+        --tertiary: #BE0E75;
+        --black: #070707;
+        --light: #EFEFEF;
+        --dark: #141414;
+        --white: #FFFFEF;
+    }
+`;
